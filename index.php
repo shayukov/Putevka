77d08858b81364e1cac9c0928ac6b0d28516cadc
shayukov <shaykov.rafai@gmail.com>
2023-02-17 13:52:51 +0300

    start

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Путевой лист</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet icon" href="img/favicon.ico">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <?php include_once "put3.php"?>
+
+  <header class="header">
+    <h1>Заполнение путевого листа</h1>
+  </header>
+  <section class="filling-form">
+    <form action="index.php" method="POST" class="form">
+      <label for="name">Введите километраж</label>
+      <input type="number" placeholder="км" maxlength="3" name="km">
+      <br>
+      <br>    
+      <label for="">Мойка</label>
+      <select name="moika-adress" id="washing" class="adress">
+        <option>Нет</option>
+        <option>Московская 43</option>
+        <option></option>
+      </select>
+
+      <br>
+      <br>    
+      <label for="">Заправка</label>
+      <select name="adress" id="refueling" class="adress">
+        <option>Нет</option>
+        <option>Девятаева 15а</option>
+        <option>Сары Садыковой 65</option>
+        <option>Отрадная 5а</option>
+      </select>
+      <br>
+      <label>
+        <input type="time" name="refillTime" id="refillTime" class="refTime">Время заправки
+      </label>
+      <br>
+      <br>
+      <label>
+        <input type="checkbox" name="friday" id="friday" value="yes" class="refTime">Пятница
+      </label>
+      <br>
+      <br>    
+      <input class="Go" type="submit" value="Поехали" >    
+      <br>
+      <br>
+      <div class="conteiner">
+        <div id="put" class="put">
+          <?php 
+            for($p=0; $p<count($arr_file); $p++) {
+              echo $arr_file[$p] . '<br><br>';
+            }
+          ?>Всего пройдено километров: <? echo $sumkm ?>
+
+        </div>
+        <div id="time-put" class="put">
+          <?php 
+            for($p=0; $p<count($begin_time_arr); $p+=4) {
+              $t = date('H:i',$begin_time_arr[$p]);
+              $t2 = date('H:i',$begin_time_arr[$p+1]);
+              $t3 = date('H:i',$begin_time_arr[$p+2]);
+              $t4 = date('H:i',$begin_time_arr[$p+3]);
+              echo $t . '--' . $t2 . '<br>'; 
+              echo $t3 . '--' . $t4 . '<br><br>'; 
+            }            
+          ?>
+        </div>
+      </div>
+    </form>
+  </section>
+</body>
+</html>
---
+++ put3.php
@@ -0,0 +1,273 @@
+<?php
+
+  $file2 = "putevka2.txt";
+  $file = file("putevka.txt");
+  $file2_arr = file($file2);
+  $file_plus = file("putevka_kmplus.txt");
+
+  //создаем массив для хранения адресов, с последующим их удалением
+  $arr_file = [];
+
+  $file_km = [18, 20, 14, 24, 18, 20, 12, 18, 14, 20, 16, 28, 16, 26, 18, 20, 12, 16, 20, 30, 14, 20, 22, 28, 20, 16, 46, 8, 8, 6, 4, 2, 10, 4, 6, 8, 8, 10];
+  $file_km_plus = [19, 21, 15, 25, 19, 21, 13, 19, 15, 21, 17, 29, 17, 27, 19, 21, 13, 17, 21, 31, 15, 21, 23, 29, 21, 17, 47, 9, 9, 7, 5, 3, 11, 5, 7, 9, 9, 11]; 
+
+  $i = 0;
+  $sumkm = 0;
+  $km = (int)$_POST['km'];
+
+  function filePutContents($i) {
+    global $file2, $file, $file2_arr;
+    file_put_contents($file2, $file[$i], FILE_APPEND);
+    $file2_arr = file($file2);
+  }
+
+  $fri = $_POST['friday'];
+  $begin = "08:40";
+  $begin_time = strtotime($begin);
+  if($fri == 'yes') {
+    $end = "16:10";
+  } else $end = "17:20";
+  $end_time = strtotime($end);
+  $lunch = "12:00";
+  $lunch_time = strtotime($lunch);
+  $lunch_end = "12:45";
+  $lunch_end_time = strtotime($lunch_end);
+
+  $begin_time_arr = [];
+  $km_arr = [];//массив для хранения километров пройденных в пути
+  $minute = [];//массив для хранения времени потраченных в пути
+
+
+  //выставляем значение мойка
+  if($_POST['moika-adress'] == "Московская 43"){
+    $sumkm += 6;
+  }
+  //выставляем значение заправка
+  if($_POST['adress'] == "Девятаева 15а") {
+    $sumkm += 8;
+    $arr_file[] = 'Пушкина8-Девятаева15А=4<br>Девятаева15А-Пушкина8=4';
+    $km_arr[] = 8;
+  } elseif($_POST['adress'] == "Сары Садыковой 65") {
+    $sumkm += 8;
+    $arr_file[] = 'Пушкина8-СарыСадыковой65=4<br>СарыСадыковой65-Пушкина8=4';
+    $km_arr[] = 8;
+  } elseif($_POST['adress'] == "Отрадная 5а") {
+    $sumkm += 14;
+    $arr_file[] = 'Пушкина8-Отрадная5а=7<br>Отрадная5а-Пушкина8=7';
+    $km_arr[] = 14;
+  }
+  //проверяем значение километраж на чётность
+  if($km % 2 != 0) {
+    for($i; $i<count($file_km_plus); $i++) {
+      if(in_array($file[$i], $file2_arr)) {
+        continue;
+      }else {
+        $kmplus = $file_km_plus[$i];
+        if ($kmplus > $km) {
+          continue;
+        } else {
+          $arr_file[] = $file_plus[$i];
+          $km_arr[] = $file_km[$i];
+          $sumkm += $kmplus;
+          filePutContents($i);
+
+          break;
+        }  
+      }
+    }
+  }
+
+  while($sumkm < $km) {
+    if($i==27) {
+      $i = 0;
+      file_put_contents($file2, '');
+      $file2_arr = file($file2);
+      continue;
+    }
+    if(in_array($file[$i], $file2_arr)) {
+      $i++;
+      continue;
+    }
+    $sumkm += $file_km[$i];
+    if($sumkm < $km) {
+      filePutContents($i);
+      $arr_file[] = $file[$i];
+      $km_arr[] = $file_km[$i];
+    } elseif($sumkm == $km) {
+      filePutContents($i);
+      $arr_file[] = $file[$i];
+      $km_arr[] = $file_km[$i];
+      break;
+    } 
+    else {
+      $sumkm -= $file_km[$i];
+      $raz = $km - $sumkm;
+      for($a=0; $a < count($file_km); $a++) {
+        if($file_km[$a] == $raz) {
+          $sumkm += $raz;
+          $arr_file[] = $file[$a];
+          $km_arr[] = $file_km[$a];
+          break;
+        }
+      } 
+      break;         
+    }
+    $i++;
+  }
+  if($_POST['moika-adress'] == 'Московская 43') {
+    $arr_file[] = 'Пушкина8-Московская43=3<br>Московская43-Пушкина8=3';
+    $km_arr[] = 6;
+  }
+
+
+  //формирование времени потраченного на поездку
+if($sumkm>0) {
+  for($r=0;$r<count($km_arr);$r++) {
+    if($km_arr[$r]>=2 && $km_arr[$r]<=9){
+      $m="00:05";
+    } elseif($km_arr[$r]>=10 && $km_arr[$r]<=12){
+      $m = "00:10";
+    } elseif($km_arr[$r]>=13 && $km_arr[$r]<=17){
+      $m = "00:15";
+    } elseif($km_arr[$r]==18){
+      $m = "00:20";
+    } elseif($km_arr[$r]>=19 && $km_arr[$r]<=20){
+      $m = "00:25";
+    } elseif($km_arr[$r]>=21 && $km_arr[$r]<=25){
+      $m = "00:30";
+    } elseif($km_arr[$r]>=26 && $km_arr[$r]<=30){
+      $m = "00:35";
+    } elseif($km_arr[$r]>=31){
+      $m = "00:40";
+    }
+    $minute[] = strtotime($m) - strtotime("00:00:00");
+  }
+  $wait = 300;
+  $wait2 = 300;
+  $coun=count($minute);
+
+  $coun2=intdiv($coun, 2);
+
+  $ref = strtotime($_POST["refillTime"]);
+  if($ref!=false) {
+    if($ref<$lunch_time) {
+      $b_t = $begin_time;
+      for($v=0; $v<$coun2; $v++) {
+        $b_t_s = $minute[$v+1] + $wait + $minute[$v+1] + $wait2;
+        $b_t += $b_t_s;
+        if($b_t>$ref-300) {
+          break;
+        }else {
+          list($minute[$v],$minute[$v+1]) = array($minute[$v+1],$minute[$v]);
+          list($arr_file[$v],$arr_file[$v+1]) = array($arr_file[$v+1],$arr_file[$v]);    
+        }
+      }
+    }elseif($ref>$lunch_end_time) {
+      list($minute[0],$minute[$coun2]) = array($minute[$coun2],$minute[0]);
+      list($arr_file[0],$arr_file[$coun2]) = array($arr_file[$coun2],$arr_file[0]);    
+      $b_t = $lunch_end_time;
+      for($v=$coun2; $v<$coun; $v++) {
+        $b_t_s = $minute[$v+1] + $wait + $minute[$v+1] + $wait2;
+        $b_t += $b_t_s;
+        if($b_t>$ref-300) {
+        //   list($minute[$v],$minute[$v+1]) = array($minute[$v+1],$minute[$v]);
+        //   list($arr_file[$v],$arr_file[$v+1]) = array($arr_file[$v+1],$arr_file[$v]);    
+          break;
+        }else {
+          list($minute[$v],$minute[$v+1]) = array($minute[$v+1],$minute[$v]);
+          list($arr_file[$v],$arr_file[$v+1]) = array($arr_file[$v+1],$arr_file[$v]);    
+        }
+      }
+    }
+  }
+
+  //до обеда
+  $bt = $begin_time;
+
+  for($u=0; $u<$coun2; $u++) {
+    $bt += $minute[$u];
+    $bt += $wait;
+    $bt += $minute[$u];
+    $bt += $wait2;
+  }
+  if($bt>$lunch_time) {
+    $x = $bt - $lunch_time;
+    $x = intdiv($x, $coun2*2);
+    for($u=0; $u<$coun2; $u++) {
+      $minute[$u]-=$x;
+    }
+  }elseif($bt<$lunch_time) {
+    $x = $lunch_time - $bt;
+    $x = intdiv($x, $coun2);
+    $x = intdiv($x, 4);
+    $wait+=$x;
+    $wait2+=$x;
+    for($u=0; $u<$coun2; $u++) {
+      $minute[$u]+=$x;
+    }
+  }
+  for($u=0; $u<$coun2; $u++) {
+    $m_u = $minute[$u];
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $m_u;
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $wait;
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $m_u;
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $wait2;
+  }
+
+  //после обеда
+  $bt = $lunch_end_time;
+  for($u=$coun2; $u<$coun; $u++) {
+    $bt += $minute[$u];
+    $bt += $wait;
+    $bt += $minute[$u];
+    $bt += $wait2;
+  }
+
+  $coun3 = $coun - $coun2;
+
+  if($bt>$end_time) {
+    $x = $bt - $end_time;
+    $x = intdiv($x, $coun3*2);
+    for($u=$coun2; $u<$coun; $u++) {
+      $minute[$u]-=$x;
+    }
+  }elseif($bt<$end_time) {
+    $x = $end_time - $bt;
+    $x = intdiv($x, $coun3);
+    $x = intdiv($x, 4);
+    $wait+=$x;
+    $wait2+=$x;
+    for($u=$coun2; $u<$coun; $u++) {
+      $minute[$u]+=$x;
+    }
+  }
+
+  $begin_time = $lunch_end_time;
+
+  for($u=$coun2; $u<$coun; $u++) {
+    $m_u = $minute[$u];
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $m_u;
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $wait;
+    // if($u==$coun-1) {
+    //   $begin_time += $m_u + $wait2;
+    //   $begin_time_arr[] = $begin_time;
+    // }else {
+    //   $begin_time += $m_u;
+    //   $begin_time_arr[] = $begin_time;
+    //   $begin_time += $wait2;
+    // }
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $m_u;
+    $begin_time_arr[] = $begin_time;
+    $begin_time += $wait2;
+  }
+
+} else echo "<html><h1> ВВЕДИТЕ КИЛОМЕТРАЖ  </h1></html>";
+
+?>
